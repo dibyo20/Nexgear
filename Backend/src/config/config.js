c66988cb23390c00b1c63ed5826f0e68124598af
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-23 17:10:40 +0530

    Add JWT_SECRET to config and validation checks

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -6,6 +6,11 @@ if (!process.env.MONGO_URI) {
     throw new Error("Please set your MONGO_URI environment variable");
 }
 
+if (!process.env.JWT_SECRET) {
+    throw new Error("Please set your JWT_SECRET environment variable");
+}
+
 export const config = {
     MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
 };
