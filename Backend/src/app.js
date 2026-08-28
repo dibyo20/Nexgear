20b209662b72d19723544ac820aa14ac61cd93a1
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-28 18:10:23 +0530

    feat: add Google OAuth authentication with passport and update dependencies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,27 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { config } from "./config/config.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(passport.initialize());
 
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/api/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+    done(null, profile);
+})
+)
+
+// Health Route
 app.get("/", (req, res) => {
     res.status(200).json({
         success: true,
@@ -15,6 +29,7 @@ app.get("/", (req, res) => {
     });
 });
 
+// Routes
 app.use("/api/auth", authRouter);
 
 export default app;
