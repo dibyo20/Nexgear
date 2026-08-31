15f1d74ca8db2f88b90b9157a9a22e9509166335
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-31 23:04:13 +0530

    feat: Corrected authentication controller for googlecallback

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -47,10 +47,10 @@ export const register = async (req, res) => {
 }
 
 export const googleCallback = async (req, res) => {
-    const { id, displaynName, emails, photos } = req.users;
+    const { id, displayName, emails, photos } = req.user;
 
     const email = emails[0].value;
-    const profilePic = photo[0].value;
+    const profilePic = photos[0].value;
 
     let user = await userModel.findOne({ email });
 
