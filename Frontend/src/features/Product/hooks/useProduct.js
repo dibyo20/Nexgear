76638e93ede6227588e779dbfd45c8d808525148
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-11 19:17:01 +0530

    feat: implement product management hooks and API service

---
+++ Frontend/src/features/Product/hooks/useProduct.js
@@ -0,0 +1,37 @@
+import { useDispatch } from "react-redux"
+import { addProductVariant, createProduct, getAllProducts, getProductById, getSellerProducts } from "../service/product.api.js";
+import { setProducts, setSellerProducts } from "../state/product.slice.js";
+
+export const useProduct = () => {
+    const dispatch = useDispatch();
+
+    async function handleCreateProduct(formData) {
+        const data = await createProduct(formData)
+        return data.product;
+    }
+
+    async function handleGetSellerProduct() {
+        const data = await getSellerProducts();
+        dispatch(setSellerProducts(data.products));
+        return data.products;
+    }
+
+    async function handleGetAllProducts() {
+        const data = await getAllProducts();
+        dispatch(setProducts(data.products))
+    }
+
+    async function handleGetProductById(productId) {
+        const data = await getProductById(productId);
+        return data.product;
+    }
+
+    async function handleAddProductVariant(productId, newProductVariant) {
+        const data = await addProductVariant(productId, newProductVariant);
+        return data;
+    }
+
+    return {
+        handleCreateProduct, handleGetSellerProduct, handleGetAllProducts, handleGetProductById, handleAddProductVariant
+    }
+}
---
+++ Frontend/src/features/Product/service/product.api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const productApiInstance = axios.create({
+    baseURL: "/api/products",
+    withCredentials: true,
+})
+
+export async function createProduct(formData) {
+    const response = await productApiInstance.post("/", formData);
+    return response.data;
+}
+
+export async function getSellerProducts() {
+    const response = await productApiInstance.get("/seller");
+    return response.data;
+}
+
+export async function getAllProducts() {
+    const response = await productApiInstance.get("/");
+    return response.data;
+}
+
+export async function getProductById(productId) {
+    const response = await productApiInstance.get(`/details/${productId}`);
+    return response.data;
+}
+
+export async function addProductVariant(productId, newProductVariant){
+    const formData = new FormData();
+
+    newProductVariant.images.forEach((image) => {
+        formData.append("images", image.file);
+    });
+
+    formData.append("stock", newProductVariant.stock);
+    formData.append("priceAmount", newProductVariant.priceAmount);
+    formData.append("attributes", JSON.stringify(newProductVariant.attributes));
+
+    const response = await productApiInstance.post(`/${productId}/variants`, formData);
+    return response.data;
+}
