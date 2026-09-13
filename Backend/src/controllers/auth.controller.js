6f4cc0190d367fe75eed70dd00401c31a3f51f72
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-13 17:53:12 +0530

    feat(auth): implement login functionality with password validation and token response

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,11 +1,16 @@
 import { config } from "../config/config.js";
 import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 async function sendTokenResponse(user, res, message) {
     const token = jwt.sign({ id: user._id }, config.JWT_SECRET, { expiresIn: "1d" });
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: config.NODE_ENV === "production",
+        sameSite: "lax",
+    });
 
     res.status(200).json({
         message,
@@ -68,7 +73,35 @@ export const googleCallback = async (req, res) => {
         { expiresIn: "7d" }
     );
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+    });
 
     res.redirect("http://localhost:5173/");
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" });
+        }
+
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const isMatch = bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        await sendTokenResponse(user, res, "Login Successful");
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
 }
