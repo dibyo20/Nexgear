e94c356ca8640d5b77724e49b6b3f560cedd9ee5
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-13 18:30:59 +0530

    feat(auth): add login, getMe, and logout routes to authentication API

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,18 +1,25 @@
 import { Router } from "express";
-import { register, googleCallback } from "../controllers/auth.controller.js";
+import { register, googleCallback, login, getMe, logout } from "../controllers/auth.controller.js";
 import { validateRegisterUser } from "../validator/auth.validator.js";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
 import passport from "passport";
+import { config } from "../config/config.js";
 
 const router = Router();
 
 router.post("/register", validateRegisterUser, register);
+router.post("/login", login);
+router.get("/me", authenticateUser, getMe);
+router.post("/logout", logout);
 
 router.get("/google",
     passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
-router.get("/google/callback",
-    passport.authenticate("google", { session: false }),
+router.get("/google/callback", passport.authenticate("google", {
+    session: false,
+    failureRedirect: config.NODE_ENV == "development" ? "http://localhost:5173/login" : "/login"
+}),
     googleCallback
 );
 
