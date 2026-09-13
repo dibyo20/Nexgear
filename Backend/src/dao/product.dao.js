df096a0076053c15cd1bfae22e25ba76c05ee222
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-13 18:15:23 +0530

    feat(dao): implement stockOfVariant function to retrieve stock for a specific product variant

---
+++ Backend/src/dao/product.dao.js
@@ -0,0 +1,16 @@
+import productModel from "../models/product.model.js";
+
+export const stockOfVariant = async (productId, variantId) => {
+    const product = await productModel.findOne({
+        id: productId,
+        "variants._id": variantId,
+    });
+
+    if (!product) {
+        return null;
+    }
+
+    const stock = product.variants.find((variant) => variant._id.toString() === variantId.toString()).stock;
+
+    return stock;
+}
