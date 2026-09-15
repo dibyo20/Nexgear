f138644c4df6589d6fd67001e1ff2d7893bc48e3
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-15 18:49:38 +0530

    feat(cart): implement getCart endpoint to retrieve user's cart

--- Backend/src/controllers/cart.controller.js
+++ Backend/src/controllers/cart.controller.js
@@ -72,3 +72,18 @@ export const addToCart = async (req, res) => {
         message: "Item added to cart"
     });
 }
+
+export const getCart = async (req, res) => {
+    const user = req.user;
+    let cart = await cartModel.findOne({ user: user._id }).populate("items.product").populate("items.variant");
+
+    if (!cart) {
+        cart = await cartModel.create({ user: user._id });
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: "Cart fetched successfully",
+        cart
+    });
+}
--- Backend/src/routes/cart.routes.js
+++ Backend/src/routes/cart.routes.js
@@ -1,11 +1,27 @@
 import express from 'express';
 import { authenticateUser } from '../middlewares/auth.middleware.js';
-import { addToCart } from '../controllers/cart.controller.js';
+import { addToCart, getCart } from '../controllers/cart.controller.js';
+import { validateAddToCart } from '../validator/cart.validator.js';
 
 const router = express.Router();
 
 router.use(authenticateUser);
 
-router.post('/:productId/:variantId', addToCart);
+/**
+ * @route POST /api/cart/add/:productId/:variantId
+ * @description Add a product to the cart
+ * @access Private
+ * @argument productId - ID of the product to be added to the cart
+ * @argument variantId - ID of the variant of the product to be added to the cart
+ * @argument quantity - Quantity of the product to be added to the cart
+ */
+router.post('/add/:productId/:variantId', validateAddToCart, addToCart);
+
+/**
+ * @route GET /api/cart/
+ * @description Get the cart
+ * @access Private
+ */
+router.get('/', getCart);
 
 export default router;
