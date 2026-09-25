d1b892ce254c7a561ae7cce1bc2eb319ab8eaaf8
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-09-25 20:22:39 +0530

    made the cart be unique for each buyer

--- Backend/src/models/cart.model.js
+++ Backend/src/models/cart.model.js
@@ -5,7 +5,8 @@ const cartSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "user",
-        required: true
+        required: true,
+        unique: true
     },
     items: [
         {
